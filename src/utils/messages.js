08fd211e3235a1e3cb5f133c21e1b1a1c9410bfb
Alchamyst <ashley@ashley-yetman.co.uk>
2020-06-18 18:19:41 +0100

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,22 @@
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    };
+};
+
+const generateLocationMessage = (username, coords) => {
+    const url = `https://google.com/maps?q=${coords.latitude},${coords.longitude}`;
+    
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    };
+};
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+};
